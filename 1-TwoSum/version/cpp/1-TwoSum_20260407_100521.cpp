f7a7b16a98845ffa81bf0fceba0bcfb9a47d3ade
Jay Patel <jay.patel.a.cg@gmail.com>
2026-04-07 10:05:24 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/cpp/1-TwoSum_20260407_100521.cpp
@@ -0,0 +1,21 @@
+// Last updated: 07/04/2026, 10:05:21
+1class Solution {
+2public:
+3    int majorityElement(vector<int>& nums) {
+4        unordered_map <int, int> res;
+5
+6        for(int i = 0; i<nums.size(); i++){
+7            res[nums[i]] = res[nums[i]] + 1;
+8        }
+9
+10        int majority = nums.size()/2;
+11
+12        for(auto it : res){
+13            if(it.second > majority){
+14                return it.first;
+15            }
+16        }
+17        return -1;
+18    };
+19
+20};
