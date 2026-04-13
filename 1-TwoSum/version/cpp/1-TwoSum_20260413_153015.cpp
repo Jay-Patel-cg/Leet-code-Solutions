69ebef3e0795495c4a0d906d05f8735b123c11f3
Jay Patel <jay.patel.a.cg@gmail.com>
2026-04-13 15:30:22 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/cpp/1-TwoSum_20260413_153015.cpp
@@ -0,0 +1,15 @@
+// Last updated: 13/04/2026, 15:30:15
+1class Solution {
+2public:
+3    int removeDuplicates(vector<int>& nums) {
+4        int i =0;
+5
+6        for(int j=1; j<nums.size(); j++){
+7            if(nums[i] != nums[j]){
+8                i++;
+9                nums[i] = nums[j];
+10            }
+11        }
+12        return i+1;
+13    }
+14};
