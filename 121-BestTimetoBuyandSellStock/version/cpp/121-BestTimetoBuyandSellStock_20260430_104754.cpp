c9f7cd40504fac849516419bc5c886716146adf6
Jay Patel <jay.patel.a.cg@gmail.com>
2026-04-30 10:47:59 +0530

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/version/cpp/121-BestTimetoBuyandSellStock_20260430_104754.cpp
@@ -0,0 +1,17 @@
+// Last updated: 30/04/2026, 10:47:54
+// Best Time to Buy and Sell Stock -> Problem number 121 in this max and min things
+1class Solution {
+2public:
+3    int maxProfit(vector<int>& prices) {
+4         
+5        int minPrice = INT_MAX;
+6        int maxProfit = 0;
+7
+8        for(int price : prices) {
+9            if(price < minPrice) minPrice = price;
+10            int profit = price - minPrice;
+11            if(profit > maxProfit) maxProfit = profit;
+12        }
+13        return maxProfit;
+14    }
+15};
