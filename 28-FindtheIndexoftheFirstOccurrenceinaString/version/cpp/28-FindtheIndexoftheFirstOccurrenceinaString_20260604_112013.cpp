524107c7394f946fd4c07cd08c08176d1f1d006f
Jay Patel <jay.patel.a.cg@gmail.com>
2026-06-04 11:20:14 +0530

    Create 28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/version/cpp/28-FindtheIndexoftheFirstOccurrenceinaString_20260604_112013.cpp
@@ -0,0 +1,8 @@
+// Last updated: 04/06/2026, 11:20:13
+// Find the Index of the First Occurrence in a String - 28 (C++) (Easy One line code)
+1class Solution {
+2public:
+3    int strStr(string haystack, string needle) {
+4        return haystack.find(needle);
+5    }
+6};
