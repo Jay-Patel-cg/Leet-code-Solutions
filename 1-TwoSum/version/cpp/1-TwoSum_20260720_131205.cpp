82de20c43dc666db8f3bc8eed278881a26d4312b
Jay Patel <jay.patel.a.cg@gmail.com>
2026-07-20 13:12:10 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/cpp/1-TwoSum_20260720_131205.cpp
@@ -0,0 +1,14 @@
+// Last updated: 20/07/2026, 13:12:05
+// Rotate Array - 189
+1class Solution {
+2public:
+3    void rotate(vector<int>& nums, int k) {
+4
+5        int n = nums.size();
+6        k = k % n;
+7
+8        reverse(nums.begin(), nums.end());
+9        reverse(nums.begin(), nums.begin() + k);
+10        reverse(nums.begin() + k, nums.end());
+11    }
+12};
