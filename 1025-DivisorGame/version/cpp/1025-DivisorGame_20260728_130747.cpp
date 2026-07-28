fa1b1102ab25ad73b41c42e3228a3aea9fe45d1f
Jay Patel <jay.patel.a.cg@gmail.com>
2026-07-28 13:08:02 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/version/cpp/1025-DivisorGame_20260728_130747.cpp
@@ -0,0 +1,12 @@
+// Last updated: 28/07/2026, 13:07:47
+// Divisor Game - 1025
+1class Solution {
+2public:
+3    bool divisorGame(int n) {
+4        if(n%2==0){
+5            return true;
+6        }else{
+7            return false;
+8        }
+9    }
+10};
