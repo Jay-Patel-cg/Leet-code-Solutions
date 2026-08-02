af7afc4b78915cda71f385dcb757475a0be8eb2f
Jay Patel <jay.patel.a.cg@gmail.com>
2026-08-02 22:01:46 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/cpp/1-TwoSum_20260802_220128.cpp
@@ -0,0 +1,16 @@
+// Last updated: 02/08/2026, 22:01:28
+// Contains Duplicate - 217
+1class Solution {
+2public:
+3    bool containsDuplicate(vector<int>& nums) {
+4        sort(nums.begin(), nums.end());
+5
+6        for (int i = 1; i < nums.size(); i++) {
+7            if (nums[i] == nums[i - 1]) {
+8                return true;
+9            }
+10        }
+11
+12        return false;
+13    }
+14};
