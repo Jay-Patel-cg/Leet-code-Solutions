240244a56068354b4177b1cf43036def66d8e4d0
Jay Patel <jay.patel.a.cg@gmail.com>
2026-08-04 13:56:41 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/cpp/1-TwoSum_20260804_135640.cpp
@@ -0,0 +1,31 @@
+// Last updated: 04/08/2026, 13:56:40
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* deleteDuplicates(ListNode* head) {
+14         if(head == nullptr){
+15            return head;
+16        }
+17        ListNode* current = head;
+18
+19        while(current-> next != nullptr){
+20            if(current->val == current->next->val){
+21                ListNode* duplicate = current->next;
+22                current->next = current->next->next;
+23                delete duplicate;
+24            } else{
+25                current= current->next;
+26            }
+27        }
+28        return head;
+29    }
+30};
