da36f03651395dcf1a21f98f840bd8733b47deea
Jay Patel <jay.patel.a.cg@gmail.com>
2026-09-02 10:30:48 +0530

    Create 3875-ConstructUniformParityArrayI

---
+++ 3875-ConstructUniformParityArrayI/version/cpp/3875-ConstructUniformParityArrayI_20260902_103044.cpp
@@ -0,0 +1,8 @@
+// Last updated: 02/09/2026, 10:30:44
+// 3875. Construct Uniform Parity Array I
+1class Solution {
+2public:
+3    bool uniformArray(vector<int>& nums1) {
+4        return true;
+5    }
+6};
